55aa550e94d09e18cd5444e0716aa78f334fdbd9
TianHeYJB <1817201531@qq.com>
2014-11-25 23:25:59 +0800

    add

--- qdialog/sortdialog.cpp
+++ qdialog/sortdialog.cpp
@@ -1,11 +1,65 @@
+#include <QtGui>
 #include "sortdialog.h"
 
 SortDialog::SortDialog(QWidget *parent)
     : QDialog(parent)
 {
+    groupOne = new QGroupBox(tr("Group One"));
+
+    QGridLayout* gridLayout1 = new QGridLayout;
+    labelLine1 = new QLabel(tr("line1"));
+    textLine1 = new QLineEdit;
+    labelLine2 = new QLabel(tr("line2"));
+    textLine2 = new QLineEdit;
+    gridLayout1->addWidget(labelLine1, 0, 0);
+    gridLayout1->addWidget(textLine1, 0, 1);
+    gridLayout1->addWidget(labelLine2, 1, 0);
+    gridLayout1->addWidget(textLine2, 1, 1);
+    groupOne->setLayout(gridLayout1);
+
+    groupTwo = new QGroupBox(tr("Group Two"));
+
+    QGridLayout* gridLayout2 = new QGridLayout;
+    label2Line1 = new QLabel(tr("line1"));
+    text2Line1 = new QLineEdit;
+    label2Line2 = new QLabel(tr("line2"));
+    text2Line2 = new QLineEdit;
+    gridLayout2->addWidget(label2Line1, 0, 0);
+    gridLayout2->addWidget(text2Line1, 0, 1);
+    gridLayout2->addWidget(label2Line2, 1, 0);
+    gridLayout2->addWidget(text2Line2, 1, 1);
+    groupTwo->setLayout(gridLayout2);
+
+    buttonOK = new QPushButton(tr("OK"));
+    buttonCancel = new QPushButton(tr("Cancel"));
+    buttonMore = new QPushButton(tr("More"));
+    QVBoxLayout* vboxBtnLayout = new QVBoxLayout;
+    vboxBtnLayout->addWidget(buttonOK);
+    vboxBtnLayout->addWidget(buttonCancel);
+    vboxBtnLayout->addStretch();
+    vboxBtnLayout->addWidget(buttonMore);
+
+    QGridLayout* mainGridLayout = new QGridLayout;
+    mainGridLayout->addWidget(groupOne,0,0);
+    mainGridLayout->addLayout(vboxBtnLayout,0,1);
+    mainGridLayout->addWidget(groupTwo,1,0);
+    setLayout(mainGridLayout);
+
+//    groupTwo->hide();
+    layout()->setSizeConstraint(QLayout::SetFixedSize);
+
+    connect(buttonMore, SIGNAL(clicked()), this, SLOT(setGroupboxTwoVisible()));
 }
 
 SortDialog::~SortDialog()
 {
 
+}
+
+void SortDialog::setGroupboxTwoVisible()
+{
+    if (groupTwo->isVisible())
+        groupTwo->hide();
+    else
+        groupTwo->show();
 }
--- qdialog/sortdialog.h
+++ qdialog/sortdialog.h
@@ -3,6 +3,13 @@
 
 #include <QDialog>
 
+QT_BEGIN_NAMESPACE
+class QGroupBox;
+class QLabel;
+class QLineEdit;
+class QPushButton;
+QT_END_NAMESPACE
+
 class SortDialog : public QDialog
 {
     Q_OBJECT
@@ -10,6 +17,27 @@ class SortDialog : public QDialog
 public:
     SortDialog(QWidget *parent = 0);
     ~SortDialog();
+
+private slots:
+    void setGroupboxTwoVisible();
+
+private:
+    QGroupBox* groupOne;
+    QLabel* labelLine1;
+    QLabel* labelLine2;
+    QLineEdit* textLine1;
+    QLineEdit* textLine2;
+
+    QGroupBox* groupTwo;
+    QLabel* label2Line1;
+    QLabel* label2Line2;
+    QLineEdit* text2Line1;
+    QLineEdit* text2Line2;
+
+    QPushButton* buttonOK;
+    QPushButton* buttonCancel;
+    QPushButton* buttonMore;
+
 };
 
 #endif // SORTDIALOG_H
