e1a0da3134601b0c6a0d904c0780cff7ca633240
TianHeYJB <1817201531@qq.com>
2014-11-27 23:04:17 +0800

    add

--- thCloudDisk/thclouddisk.cpp
+++ thCloudDisk/thclouddisk.cpp
@@ -1,13 +1,55 @@
 #include "thclouddisk.h"
 #include <QtGui>
 
-ThCloudDisk::ThCloudDisk(QWidget *parent)
-    : QMainWindow(parent)
+ThCloudDisk::ThCloudDisk(bool haveTitleBar, bool haveToolBar, bool haveStatusBar, QWidget *parent)
+    : QFrame(parent)
 {
+    // init member data
     leftMousePress = false;
 
+    // set up all child widgets
+    if (haveTitleBar)
+        ;
+
+    if (haveToolBar){
+        thToolBar = new QToolBar;
+        thToolBar->setFixedHeight(40);
+    }
+    else{
+        thToolBar = 0;
+    }
+
+    thCentralWidget = new QWidget;
+
+    if (haveStatusBar){
+        thStatusBar = new QStatusBar;
+    }
+    else{
+        thStatusBar = 0;
+    }
+
+    QVBoxLayout* mainLayout = new QVBoxLayout;
+
+    // set layout
+    if ( haveTitleBar ){
+        mainLayout->addWidget(thToolBar);
+    }
+
+    mainLayout->addWidget(thCentralWidget);
+
+    if ( haveStatusBar ){
+        mainLayout->addWidget(thStatusBar);
+    }
+
+    mainLayout->setSpacing(0);
+    mainLayout->setContentsMargins(0,0,0,0);
+    setLayout(mainLayout);
+
+    // set mainwindow
     setGeometry(300, 300, 400, 300);
     setWindowFlags(Qt::FramelessWindowHint);
+    setStyleSheet("QFrame {background-color:black;border:1px solid black;}");
+
 }
 
 ThCloudDisk::~ThCloudDisk()
@@ -22,6 +64,8 @@ void ThCloudDisk::mousePressEvent(QMouseEvent *event)
         leftMousePress = true;
         leftTopPosition = event->pos();
     }
+
+    QFrame::mousePressEvent(event);
 }
 
 void ThCloudDisk::mouseReleaseEvent(QMouseEvent *event)
@@ -30,6 +74,8 @@ void ThCloudDisk::mouseReleaseEvent(QMouseEvent *event)
     {
         leftMousePress = false;
     }
+
+    QFrame::mouseReleaseEvent(event);
 }
 
 void ThCloudDisk::mouseMoveEvent(QMouseEvent *event)
@@ -38,4 +84,19 @@ void ThCloudDisk::mouseMoveEvent(QMouseEvent *event)
     {
         move(event->globalPos() - leftTopPosition);
     }
+
+    QFrame::mouseMoveEvent(event);
+}
+
+void ThCloudDisk::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Escape:
+        qApp->quit();
+        break;
+    default:
+        break;
+    }
+
+    QFrame::keyPressEvent(event);
 }
--- thCloudDisk/thclouddisk.h
+++ thCloudDisk/thclouddisk.h
@@ -1,24 +1,38 @@
 #ifndef THCLOUDDISK_H
 #define THCLOUDDISK_H
 
-#include <QMainWindow>
+#include <QFrame>
 
-class ThCloudDisk : public QMainWindow
+QT_BEGIN_NAMESPACE
+class QToolBar;
+class QStatusBar;
+class QWidget;
+class QVBoxLayout;
+QT_END_NAMESPACE
+
+// http://www.cnblogs.com/appsucc/archive/2012/03/28/2421225.html
+class ThCloudDisk : public QFrame
 {
     Q_OBJECT
 
 public:
-    ThCloudDisk(QWidget *parent = 0);
+    ThCloudDisk(bool haveTitleBar = false, bool haveToolBar = false, bool haveStatusBar = false, QWidget *parent = 0);
     ~ThCloudDisk();
 
 protected:
-    void mousePressEvent(QMouseEvent* event);
-    void mouseReleaseEvent(QMouseEvent* event);
-    void mouseMoveEvent(QMouseEvent* event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void keyPressEvent(QKeyEvent *event);
 
 private:
     bool    leftMousePress;
     QPoint  leftTopPosition;
+
+    QToolBar    *thToolBar;
+    QStatusBar  *thStatusBar;
+    QWidget     *thCentralWidget;
+//    QVBoxLayout* mainLayout;
 };
 
 #endif // THCLOUDDISK_H
