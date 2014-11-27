8b3206ea216d83d89d0df5555fc2f1789e01eac6
TianHeYJB <1817201531@qq.com>
2014-11-27 10:09:59 +0800

    add

--- mwApplication/mainwindow.cpp
+++ mwApplication/mainwindow.cpp
@@ -142,13 +142,25 @@ bool MainWindow::maybeSave()
         ret = QMessageBox::warning(this, tr("Information"),
                                    tr("The document has been modified.\nDo you want to save your changes?"),
                                    QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        if (QMessageBox::Save == ret)
+            return save();
+        else if (QMessageBox::Cancel == ret)
+            return false;
     }
     return true;
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
 {
-    writeSettings();
+    if (maybeSave())
+    {
+        writeSettings();
+        event->accept();
+    }
+    else
+    {
+        event->ignore();
+    }
 }
 
 void MainWindow::setCurrentFile(const QString &fileName)
@@ -162,3 +174,73 @@ void MainWindow::setCurrentFile(const QString &fileName)
         shownNaem = "untitled.txt";
     setWindowFilePath(shownNaem);
 }
+
+bool MainWindow::save()
+{
+    if (curFile.isEmpty())
+    {
+        return saveAs();
+    }
+    else
+    {
+        return saveFile(curFile);
+    }
+}
+
+bool MainWindow::saveAs()
+{
+    QString fileName=QFileDialog::getSaveFileName(this);
+    if (fileName.isEmpty())
+        return false;
+    return saveFile(fileName);
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, tr("Application"),
+                             tr("Cannot write file %1:\n%2")
+                             .arg(fileName)
+                             .arg(file.errorString()) );
+        return false;
+    }
+
+    QTextStream out(&file);
+#ifndef QT_NO_CURSOR
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+#endif
+
+    out << textEdit->toPlainText();
+
+#ifndef QT_NO_CURSOR
+    QApplication::restoreOverrideCursor();
+#endif
+
+    setCurrentFile(fileName);
+    statusBar()->showMessage(tr("File saved"), 2000);
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
